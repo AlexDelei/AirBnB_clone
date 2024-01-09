ff4c44a2b0c943af0d939fdde101932fa3a5a802
AlexDelei <alexdelei125@gmail.com>
2024-01-09 04:37:29 -0800

    Console done

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Py Shell"""
-import sys, cmd, os, subprocess
+import sys
+import cmd
+import os
+import subprocess
+
 
 class ConsoleCode(cmd.Cmd):
     """Our Console code"""
@@ -39,6 +43,61 @@ class ConsoleCode(cmd.Cmd):
         """implemeting the vi text editor"""
 
         subprocess.run(['vi', arg])
+
+    def do_ls(self, arg):
+        """Lists all files,directories in the current dir and the specified"""
+
+        if not arg:
+            arg = '.'
+        try:
+            files = os.listdir(arg)
+            for i in files:
+                file_path = os.path.join(arg, i)
+                if os.path.isdir(file_path):
+                    print('\033[94m' + i + '\033[0m')
+                elif os.access(file_path, os.X_OK):
+                    print('\033[92m' + i + '\033[0m')
+                else:
+                    print(i)
+        except FileNotFoundError:
+            print(f'Directory not found: {arg}')
+        except PermissionError:
+            print(f'Permission denied to access {arg}')
+
+    def do_clear(self, arg):
+        """clears the screen"""
+
+        os.system('clear' if os.name == 'posix' else 'cls')
+
+    def do_chmod(self, arg):
+        """Chnages the mode of a file"""
+
+        args = arg.split()
+        if len(args) != 2:
+            print('Usage: chmod <permission> <file>')
+            return
+        permission, file_path = args
+        valid_permissions = {'u+x': 0o100, 'u-x': 0o0, 'g+x': 0o010, 'g-x'}
+
+        if permission not in valid_permissions:
+            print('Invalid permission. Use u+x, u-x, g+x, g-x')
+            return
+
+        try:
+            current_mode = os.stat(file_path).st_mode
+            if valid_permissions[permission] & current_mode == 0:
+                new_mode = current_mode | valid_permissions[permission]
+                os.chmod(file_path, new_mode)
+        except FileNotFoundError:
+            print(f'File not found: {file_path}')
+        except PermissionError:
+            print("Permission denied")
+
+    def emptyline(self):
+        """Do nothing if its empty line"""
+
+        pass
+
 
 if __name__ == '__main__':
     """Handling interactive and non-interactive mode"""
