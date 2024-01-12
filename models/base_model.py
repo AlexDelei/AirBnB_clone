2bc0bdbcf96d39f38c7255a33933c1abbf68ae89
AlexDelei <alexdelei125@gmail.com>
2024-01-12 00:53:41 -0800

    Fixed dictionary store

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """My base model module"""
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """Parent class with
@@ -24,20 +24,24 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """Saves the updated time"""
         
-        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Converts to dictionary"""
 
-        obj_dict = self.__dict__
-        obj_dict['id'] = self.id
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict = {
+                'my_number': self.my_number,
+                'name': self.name,
+                '__class__': self.__class__.__name__,
+                'updated_at': self.updated_at.isoformat(),
+                'id': self.id,
+                'created_at': self.created_at.isoformat()
+                }
         return obj_dict
     
     def __str__(self):
