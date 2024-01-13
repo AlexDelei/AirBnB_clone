16e0b4f3bc2211a63e648e6eb5891436d85e2b15
AlexDelei <alexdelei125@gmail.com>
2024-01-13 12:58:43 -0800

    inheriting fix

--- models/city.py
+++ models/city.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     state_id = ""
-    name= ""
+    name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
