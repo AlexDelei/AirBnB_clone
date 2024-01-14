ea7928471bdffbcacacb44db240676ba4e275916
AlexDelei <alexdelei125@gmail.com>
2024-01-14 04:44:45 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -17,16 +17,15 @@ class BaseModel:
         """Initializing public int attr"""
         if kwargs:
             for key, value in kwargs.items():
-                if key in ('created_at', ' updated_at'):
-                    setattr(
-                        self,
-                        key,
-                        datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                        )
-                elif key == "__class__":
-                    continue
-                else:
-                    setattr(self, key, value)
+                if key != key:
+                    if key in ('created_at', ' updated_at'):
+                        setattr(
+                            self,
+                            key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                            )
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
