081d582e5d8f0a10c56118820132e9a44f94a236
AlexDelei <alexdelei125@gmail.com>
2024-01-16 01:25:32 -0800

    Fixed time format

--- models/base_model.py
+++ models/base_model.py
@@ -27,13 +27,13 @@ class BaseModel:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
 
     def save(self):
         """Saves the updated time"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
